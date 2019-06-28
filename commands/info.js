0056c61872f9a3488fabe1d4d1772c772b154695
Xeoth <ujan.pl@hotmail.com>
2019-06-28 15:48:00 +0200

    Added info command

--- commands/info.js
+++ commands/info.js
@@ -4,6 +4,6 @@ exports.run = (client, message, args) => {
     const infoEmbed = new Discord.RichEmbed()
         .setTitle("Info about Afficom")
         .addField("Developers", "Xeoth#1842")
-        .addField("Toxicity models", "To detect toxic messages, we're using toxicity models from [here](https://github.com/tensorflow/tfjs-models/tree/master/toxicity). They're licensed under [Apache 2.0](https://raw.githubusercontent.com/tensorflow/tfjs-models/master/LICENSE).")
+        .addField("Toxicity models", "To detect toxic messages, we're using toxicity models from [tensorflow/tfjs-models](https://github.com/tensorflow/tfjs-models/tree/master/toxicity). They're licensed under [Apache 2.0](https://raw.githubusercontent.com/tensorflow/tfjs-models/master/LICENSE).")
     message.channel.send(infoEmbed)
     };
