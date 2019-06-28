b415c8505995110ba30d16fa38e8a9d3a906fb48
Xeoth <ujan.pl@hotmail.com>
2019-06-28 15:27:08 +0200

    Added a warning

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -9,5 +9,5 @@ module.exports = async (client, member) => {
     filterChannel.overwritePermissions(member.id, {
         VIEW_CHANNEL: false
     })
-    filterChannel.send("This is the channel in which all detected messages will be waiting for review.")
+    filterChannel.send("This is the channel in which all detected messages will be waiting for review. Please, __do not delete or change the name of this channel!__")
 }
