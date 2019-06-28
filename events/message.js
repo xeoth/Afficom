e81aae1b772420d2233875c40709f65e36b2d2fb
Xeoth <ujan.pl@hotmail.com>
2019-06-28 15:25:31 +0200

    Message detection works

--- events/message.js
+++ events/message.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js")
+
 module.exports = (client, message) => {
     // Ignore all bots
     if (message.author.bot) return;
@@ -7,7 +9,7 @@ module.exports = (client, message) => {
       const tf = require("@tensorflow/tfjs");
       const toxicity = require("@tensorflow-models/toxicity")
     
-      const thr = 0.1;
+      const thr = 0.75;
     
       toxicity.load(thr).then(model => {
         const sentence = message.content
@@ -22,7 +24,30 @@ module.exports = (client, message) => {
           // console.log("Threat              |", predictions[5].results[0].match)
           // console.log("Final verdict       |", predictions[6].results[0].match)
           // console.log("----------")
-          
+          if (predictions[6].results[0].match === true) {
+            //Setting the emojis
+            var names = ['identity', 'insult', 'obscene', 'severe', 'sexuallyexpl', 'threat']
+            var i;
+            for (i = 0; i < 6; i++) {
+              if (predictions[i].results[0].match === true) {
+                names[i] = "✅"
+              } else if (predictions[i].results[0].match === false) {
+                names[i] = "❎"
+              }
+            }
+            const detectedMessageEmbed = new Discord.RichEmbed()
+              .setTitle("Potentially toxic message detected!")
+              .addField("Message content", message.content)
+              .addField("Identity attack", names[0], true)
+              .addField("Insult", names[1], true)
+              .addField("Obscene", names[2], true)
+              .addField("Severely toxic", names[3], true)
+              .addField("Sexually explicit", names[4], true)
+              .addField("Threat", names[5], true)
+              .setColor("ff0000")
+            const filterChannel = message.guild.channels.find(x => x.name === "filtered-messages")
+            filterChannel.send(detectedMessageEmbed)
+          }
         })
       })
       
