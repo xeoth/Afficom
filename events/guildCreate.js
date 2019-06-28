aa56e8ae4f93be1807c50ccf4e2cbfabf8466e52
Xeoth <ujan.pl@hotmail.com>
2019-06-28 14:14:18 +0200

    Bot creates a channel on join

---
+++ events/guildCreate.js
@@ -0,0 +1,13 @@
+
+module.exports = async (client, member) => {
+    const filterChannel = await member.createChannel('filtered-messages')
+    filterChannel.overwritePermissions(client.user.id, {
+        VIEW_CHANNEL: true,
+        SEND_MESSAGES: true,
+        ADD_REACTIONS: true
+    })
+    filterChannel.overwritePermissions(member.id, {
+        VIEW_CHANNEL: false
+    })
+    filterChannel.send("This is the channel in which all detected messages will be waiting for review.")
+}
