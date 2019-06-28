cd79ef362785695917a30c66ff72c519e447ab6a
Xeoth <ujan.pl@hotmail.com>
2019-06-28 17:01:57 +0200

    Changed the color in the info command

--- commands/info.js
+++ commands/info.js
@@ -5,5 +5,6 @@ exports.run = (client, message, args) => {
         .setTitle("Info about Afficom")
         .addField("Developers", "Xeoth#1842")
         .addField("Toxicity models", "To detect toxic messages, we're using toxicity models from [tensorflow/tfjs-models](https://github.com/tensorflow/tfjs-models/tree/master/toxicity). They're licensed under [Apache 2.0](https://raw.githubusercontent.com/tensorflow/tfjs-models/master/LICENSE).")
+        .setColor("0080ff")
     message.channel.send(infoEmbed)
     };
