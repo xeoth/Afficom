2ba217fe5207ae21b26900880ea0a07444447315
Xeoth <ujan.pl@hotmail.com>
2019-06-28 17:11:24 +0200

    Updated help

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
     const p = client.config.prefix
     const helpEmbed = new Discord.RichEmbed()
-        .addField("Commands", `\`${p}info\`, \`${p}ping\`, \`${p}info\``)
+        .addField("Commands", `\`${p}info\`, \`${p}ping\`, \`${p}info\`, \`${p}server\``)
         .setFooter(`My prefix here is \"${p}\"`)
         .setColor("0080ff")
     message.channel.send(helpEmbed)
