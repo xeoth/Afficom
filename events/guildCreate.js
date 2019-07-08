5566aae26f874e79f39eabfc0b3d165f78c06d93
Xeoth <ujan.pl@hotmail.com>
2019-07-08 13:03:33 +0200

    Added checking bot permissions before creating a channel

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,6 +2,7 @@
 module.exports = async (client, member) => {
     const requestedChannel = member.channels.find(x => x.name === "afficom-filtered-messages")
     if (!requestedChannel) {
+        if (!member.me.hasPermission('MANAGE_CHANNELS')) return member.owner.send("Hello!\nThe admins of your server have added me, but it seems like I'm missing the *manage channels* permission. Please, create a channel called *#afficom-filtered-messages* (make sure I have a read and write access to it) and consider giving me the *manage channels* permission. Otherwise, the bot might not function as intended.\nThanks!");
         const filterChannel = await member.createChannel('afficom-filtered-messages')
         filterChannel.overwritePermissions(client.user.id, {
             VIEW_CHANNEL: true,
