a0b3a21e497fd6a82880993470edc8ad3a340c31
Xeoth <ujan.pl@hotmail.com>
2019-07-09 13:29:29 +0200

    Additional permission check: manage messages

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,8 +1,9 @@
+const translation = require(`./translations/english.json`)
 
 module.exports = async (client, member) => {
     const requestedChannel = member.channels.find(x => x.name === "afficom-filtered-messages")
     if (!requestedChannel) {
-        if (!member.me.hasPermission('MANAGE_CHANNELS')) return member.owner.send("Hello!\nThe admins of your server have added me, but it seems like I'm missing the *manage channels* permission. Please, create a channel called *#afficom-filtered-messages* (make sure I have a read and write access to it) and consider giving me the *manage channels* permission. Otherwise, the bot might not function as intended.\nThanks!");
+        if (!member.me.hasPermission('MANAGE_CHANNELS')) return member.owner.send(translation.missingPerms_managechannels);
         const filterChannel = await member.createChannel('afficom-filtered-messages')
         filterChannel.overwritePermissions(client.user.id, {
             VIEW_CHANNEL: true,
@@ -12,7 +13,8 @@ module.exports = async (client, member) => {
         filterChannel.overwritePermissions(member.id, {
             VIEW_CHANNEL: false
         })
-        const sentMessage = await filterChannel.send("This is the channel in which all detected messages will be waiting for review. Please, __do not delete or change the name of this channel!__")
+        const sentMessage = await filterChannel.send(translation.description_detectedChannel)
+        if (!member.me.hasPermission('MANAGE_MESSAGES')) return member.owner.send(translation.missingPerms_managemessages);
         sentMessage.pin()
     }
 }
